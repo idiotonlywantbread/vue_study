ab4e8818ba3896b086b565a37caeffc859238d21
idiotonlywantbread <17689403104@163.com>
2022-03-17 18:09:56 +0800

    Initial commit

---
+++ src/views/BookView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>编号</td>
+        <td>名字</td>
+        <td>作者</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookView",
+  created(){
+    this.$axios.get('http://localhost:8123/book/findAll/').then(function(resp){
+      console.log(resp)
+      // _this.tableData = resp.data.content
+      // _this.pageSize = resp.data.size
+      // _this.total = resp.data.totalElements
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
